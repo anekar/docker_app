dd974a48268a96fcf7296301a59fa1d255fe9afc
anekar <anestis.karonidis@gmail.com>
2023-01-11 16:03:36 +0200

    Added docker and app files

---
+++ compose/mysql/students.sql
@@ -0,0 +1,11 @@
+use db;
+
+CREATE TABLE students(
+StudentID int NOT NULL,
+First_Name varchar(100),
+Last_Name varchar(100));
+
+
+INSERT INTO students(StudentID, First_Name, Last_Name)
+VALUES(1, "Anestis", "Karonidis");
+
---
+++ compose/python/app.py
@@ -0,0 +1,2 @@
+connection=mysql.connector.connect(user='root',password='root',host='mysql',port='3306',database='db')
+print("Database connected")
